439529339c3fac16366b3f313fd6b488e305b10d
ismail-m-a <ismaaciilmuxamadaadam@gmail.com>
2024-10-22 14:02:40 +0200

    Login component

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  
+  )
+}
+
+export default Login
---
+++ src/components/Login/Login.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from "./Login";
+
+export default {
+    title: "Login/Login",
+    component: Login
+}
+
+export const Default = {
+    render: () => <Login />
+}
